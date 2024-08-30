dfea066ea0962256e2219fa9b996387baf7ad085
Natasha <ncatalan06@gmail.com>
2024-08-30 18:48:08 +0800

    Initial commit for the new repository

--- FlordeGrace/StudInfoSys/App_urls.py
+++ FlordeGrace/StudInfoSys/App_urls.py
@@ -3,5 +3,17 @@ from . import views
 
 # URL CONFIGURATION
 urlpatterns = [
-    path('', views.admin_homepage)
+    path('', views.admin_homepage),
+    path('Add_Inv.html', views.add_inventory),
+    path('Depreciated_Inv.html', views.depreciated_inventory),
+    path('DepInv_Classroom.html', views.depinv_classroom),
+    path('DepInv_Office.html', views.depinv_office),
+    path('DepInv_Library.html', views.depinv_library),
+    path('DepInv_Clinic.html', views.depinv_clinic),
+    path('Purchase_Req.html', views.purchase_request),
+    path('Eval_Req.html', views.evaluation_request),
+    path('Add_Supplier.html', views.add_supplier),
+    path('Supplier_List.html', views.supplier_list),
+    path('Equip_Maintenance.html', views.equipment_maintenance),
+    path('Archive.html', views.archive),
 ]
--- FlordeGrace/StudInfoSys/views.py
+++ FlordeGrace/StudInfoSys/views.py
@@ -2,3 +2,39 @@ from django.shortcuts import render
 
 def admin_homepage(request):
     return render(request, 'Admin_Home.html')
+
+def add_inventory(request):
+    return render(request, 'Add_Inv.html')
+
+def depreciated_inventory(request):
+    return render(request, 'Depreciated_Inv.html')
+
+def depinv_classroom(request):
+    return render(request, 'DepInv_Classroom.html')
+
+def depinv_office(request):
+    return render(request, 'DepInv_Office.html')
+
+def depinv_library(request):
+    return render(request, 'DepInv_Library.html')
+
+def depinv_clinic(request):
+    return render(request, 'DepInv_Clinic.html')
+
+def purchase_request(request):
+    return render(request, 'Purchase_Req.html')
+
+def evaluation_request(request):
+    return render(request, 'Eval_Req.html')
+
+def add_supplier(request):
+    return render(request, 'Add_Supplier.html')
+
+def supplier_list(request):
+    return render(request, 'Supplier_List.html')
+
+def equipment_maintenance(request):
+    return render(request, 'Equip_Maintenance.html')
+
+def archive(request):
+    return render(request, 'Archive.html')
